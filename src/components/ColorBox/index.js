d8759c0f6a871c8ce0f293f245ff4d25efae7f9a
HoanThuat <hoanthuatenji@gmail.com>
2020-10-26 15:11:58 +0700

    learn useState and change box color

---
+++ src/components/ColorBox/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./ColorBox.scss";
+
+ColorBox.propTypes = {};
+
+function getRandomNewColor() {
+  const color_list = ["deeppink", "green", "yellow", "black", "blue"];
+  const randomIndex = Math.trunc(Math.random() * 5);
+  return color_list[randomIndex];
+}
+
+function ColorBox() {
+  const [color, setColor] = useState(() => {
+    const initColor = localStorage.getItem("box-color") || "deeppink";
+    console.log(initColor);
+    return initColor;
+  });
+
+  function handleBoxClick() {
+    const newColor = getRandomNewColor();
+    setColor(newColor);
+    localStorage.setItem("box-color", newColor);
+  }
+
+  return (
+    <div
+      className="color-box"
+      style={{ backgroundColor: color }}
+      onClick={handleBoxClick}
+    ></div>
+  );
+}
+
+export default ColorBox;
