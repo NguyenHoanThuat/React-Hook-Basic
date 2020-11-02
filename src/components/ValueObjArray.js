0771b1ef4f0326bf90e1a8edf4e56526b80346e9
HoanThuat <hoanthuatenji@gmail.com>
2020-11-02 21:07:21 +0700

    learn javascript to value, object, array

---
+++ src/components/ValueObjArray.js
@@ -0,0 +1,70 @@
+// Tham tri
+
+let a = 10;
+let b = a;
+a = 5;
+console.log(b);
+
+//Tham chieu
+
+const a = { name: "Thuat" };
+const b = a;
+a.name = "Vi";
+console.log(b);
+
+// Truyen tham so dang tham tri
+
+const doMagic = (number) => {
+  number = 5;
+};
+
+const a = 10;
+doMagic(a);
+console.log(a);
+
+// Truyen tham so dang tham chieu
+
+const doMagic1 = (obj) => {
+  obj.name = "Vi";
+};
+
+const object1 = { name: "Thuat" };
+doMagic1(object1);
+console.log(object1);
+
+// Truyen tham so nhung khong muon dinh tham chieu
+
+const doMagic2 = (obj) => {
+  obj.name = "Vi";
+};
+
+const objectName = { name: "Thuat" };
+const newObject = { ...objectName };
+doMagic2(newObject);
+console.log(objectName);
+
+// Vd
+
+const arr = [1, 2, 3];
+const newArr = [...arr];
+newArr.push(4);
+console.log(arr);
+
+//Bai tap 1 = 1000 + 2500 = 3500
+
+const doSomeThingCome = (number, obj) => {
+  number = 1500;
+  obj.value = 2500;
+};
+
+const a = 1000;
+const b = { value: 2000 };
+doSomeThingCome(a, b);
+console.log(a + b.value);
+
+//Bai tap 2
+
+const doSomeThing2 = (obj, arr) => {};
+
+const a = { value: 1000 };
+const b = [2000];
