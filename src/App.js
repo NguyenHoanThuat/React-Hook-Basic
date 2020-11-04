20b2836aceb1afda8a9f9ab222d6f66112e34a3d
HoanThuat <hoanthuatenji@gmail.com>
2020-11-04 09:05:40 +0700

    Learn Js: first learn Js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.scss";
 import ColorBox from "./components/ColorBox";
+import Learn from "./components/Learn1/index";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Welcome to react hooks!</h1>
 
       <ColorBox />
+      <Learn />
     </div>
   );
 }
---
+++ src/components/Learn1/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Learn_1(props) {
+  alert("Hallo Javascript");
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default Learn_1;
