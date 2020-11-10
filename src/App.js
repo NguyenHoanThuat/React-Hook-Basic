be7512c74e48e0aa025125a2ab4bd9e846c0fa8c
HoanThuat <hoanthuatenji@gmail.com>
2020-11-10 16:21:32 +0700

    Learn Js: Post List

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import ColorBox from "./components/ColorBox";
+import PostList from "./components/PostList";
 
 function App() {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl =
+          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const response = await fetch(requestUrl);
+        const responseUrl = await response.json();
+        console.log(responseUrl);
+
+        const { data } = responseUrl;
+        setPostList(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchPostList();
+  }, []);
   return (
     <div className="App">
-      <h1>Welcome to react hooks!</h1>
+      <h1>React hook Post List!</h1>
 
       <ColorBox />
+      <PostList posts={postList} />
     </div>
   );
 }
