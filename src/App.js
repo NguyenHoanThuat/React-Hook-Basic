47d28888a868ab5d78313fd2894a167aec019bf6
HoanThuat <hoanthuatenji@gmail.com>
2020-11-11 15:16:35 +0700

    Learn Js: Pagination

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
 import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+
 import "./App.scss";
 import ColorBox from "./components/ColorBox";
 import PostList from "./components/PostList";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [postList, setPostList] = useState([]);
 
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1,
+  });
+
+  const [filter, setFilter] = useState({
+    _limit: 10,
+    _page: 1,
+  });
+
   useEffect(() => {
     async function fetchPostList() {
       try {
-        const requestUrl =
-          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const paramsString = queryString.stringify(filter);
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
         const response = await fetch(requestUrl);
         const responseUrl = await response.json();
         console.log(responseUrl);
 
-        const { data } = responseUrl;
+        const { data, pagination } = responseUrl;
         setPostList(data);
+        setPagination(pagination);
       } catch (error) {
         console.log(error);
       }
     }
     fetchPostList();
-  }, []);
+  }, [filter]);
+
+  const handlePageChange = (newPage) => {
+    console.log("new page ", newPage);
+    setFilter({
+      ...filter,
+      _page: newPage,
+    });
+  };
+
   return (
     <div className="App">
       <h1>React hook Post List!</h1>
 
       <ColorBox />
       <PostList posts={postList} />
+      <Pagination pagination={pagination} onPageChange={handlePageChange} />
     </div>
   );
 }
